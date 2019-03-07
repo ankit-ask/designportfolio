e4509b8ca3532d6ab62244eb1d1ac702fc932969
Ankit Singh <ankit20september@gmail.com>
2019-03-07 20:29:07 +0530

    v1

--- js/main.js
+++ js/main.js
@@ -4,3 +4,39 @@ $(document).ready(function(){
         $('#overlay').toggleClass('open');
     });
 });
+
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 20;
+
+function animate() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.landing').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(animate);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (40 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (20 * lMouseY) / 100;
+
+//   lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+//   lFollowY = (10 * lMouseY) / 100;
+
+});
+
+animate();
